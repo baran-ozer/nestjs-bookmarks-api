15a0e4df1596845428d856e0e2c9867206617ec9
baran <baran.ozer@orionfintechinc.com>
2022-12-30 17:05:38 +0300

    services

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // removes all other fields that not defined in dto classes
+    }),
+  );
   await app.listen(3333);
 }
 bootstrap();
